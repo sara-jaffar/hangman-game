4a1c2041de2a57f9d2c74710dc18f93542eb6a92
sara-jaffar <sara.alshoula55@email.com>
2025-07-01 22:24:17 +0300

    Added initial html structure

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@
 
 
 /*---------- Variables (state) ---------*/
-
+let wrongGusses;
 
 /*----- Cached Element References  -----*/
-
+const letterBtns = document.querySelectorAll(".letter-btn");
+const blankEl = document.querySelector("#blanks");
+const livesRemaningEl = document.querySelector("#lives-remaning");
+const wrongLettersEl = document.querySelector("#wrong-letters");
+const resetBtnEl = document.querySelector("#reset-btn");
 
 /*-------------- Functions -------------*/
 
